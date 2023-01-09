eac02a40e76e5d9e7bb362360c4353e63af6161a
YoussefHesham <YoussefHesham@TRUFLAEGY116>
2023-01-10 01:02:06 +0200

    Finish Header

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -1,38 +1,39 @@
-import React from 'react'
-import "./header.css"
-import people from "../../assets/people.png"
-import ai from "../../assets/ai.png"
+import React from "react";
+import "./header.css";
+import people from "../../assets/people.png";
+import ai from "../../assets/ai.png";
 
-
-
- const Header = () => {
+const Header = () => {
   return (
-    <section className='gpt3__header section__padding' id='home'>
-
-      <section className='gpt3__navbar-content'>
-
-        <h1 className='gradient__text'>
+    <section className="gpt3__header section__padding" id="home">
+      <section className="gpt3__header-content">
+        <h1 className="gradient__text">
           Let`s Build Something amazing with GPT-3 OpenAi
         </h1>
-        <p> Yet bed any for travelling assistance indulgence unpleasing. Not thoughts all exercise blessing. Indulgence way everything joy alteration boisterous the attachment. Party we years to order allow asked of.</p>
-        
-        <article className='gpt3__header-content__input'>
-          <input type="email" placeholder="Your Email"/>
-          <button  type="button">Get Started</button>
+        <p>
+          {" "}
+          Yet bed any for travelling assistance indulgence unpleasing. Not
+          thoughts all exercise blessing. Indulgence way everything joy
+          alteration boisterous the attachment. Party we years to order allow
+          asked of.
+        </p>
+
+        <article className="gpt3__header-content__input">
+          <input type="email" placeholder="Your Email" />
+          <button type="button">Get Started</button>
         </article>
 
-        <article className='gpt3__header-content__people'>
-          <img src={people} alt="people"/>
-          <p>1.600 people requested access</p>  
+        <article className="gpt3__header-content__people">
+          <img src={people} alt="people" />
+          <p>1.600 people requested access</p>
         </article>
-
-        <article className='gpt3__header-image'>
-          <img src={ai} alt="ai"/>
-        </article>
-
       </section>
-    </section>
-  )
-}
 
-export default Header
+      <article className="gpt3__header-image">
+        <img src={ai} alt="ai" />
+      </article>
+    </section>
+  );
+};
+
+export default Header;
